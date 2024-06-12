364d4f4df7972930047e0ec1b8f5ae4b2dae54c7
lachy-c <lachlan.ceely@gmail.com>
2024-06-12 11:00:13 +1000

    About me change

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -18,7 +18,7 @@ def is_odd(a_number):
 
     So if a_number modulo two is zero, then it's even.
     """
-    return None
+    return True
 
 
 def fix_it(moves=True, should_move=True):
@@ -36,7 +36,7 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements.
     As an extra challenge, see if you can get that down to three.
     """
-    return None
+    return False
 
 
 def loops_preview():
